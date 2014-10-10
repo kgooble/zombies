04435f9ee15294ba2016b8d5a971ca2c3acb4466
Shahrin Khan <gooble.sk@gmail.com>
2014-10-09 21:09:17 -0400

    Add test for sprite renderer when pose is given.

--- scripts/tests/rendererstest.js
+++ scripts/tests/rendererstest.js
@@ -43,6 +43,76 @@ function(renderers) {
         		"fillStyle set correctly in ctx after draw");
         });
 
+		test("sprite renderer test when pose given",
+		function () {
+			expect(10);
+
+			var newState = "newState";
+			var poseName = "thePose";
+			var fakeTimeDelta = 0.1;
+			var fakeAnimator = {
+				update: function (timeDelta) {
+					equal(timeDelta, fakeTimeDelta, 
+						  "timeDelta was passsed correctly to animator");
+					return newState;
+				},
+				setState: function (state) {
+					equal(state, newState, "state set correctly in animator");
+				},
+				getPoseName: function () {
+					ok(true, "called getPoseName on animator");
+					return poseName;
+				}
+
+			};
+			var fakeCtx = {
+				"name":"fakeCtx"
+			};
+			var fakeTopLeft = { "name": "faketopleft" };
+			var fakeJsonLoader = {
+				"name":"json"  // To make sure deepEqual works
+			};
+			var fakeImageLoader = {
+				"name":"image"  // To make sure deepEqual works
+			};
+			var fakeSprite = {
+				getPose: function (orientation, name, jsonLoader, imageLoader) {
+					deepEqual(jsonLoader, fakeJsonLoader, 
+						"passed jsonLoader correctly to sprite");
+					deepEqual(imageLoader, fakeImageLoader,
+						"passed imageLoader correctly to sprite");
+					deepEqual(orientation, "right", 
+						"orientation passed correctly to sprite");
+					deepEqual(name, poseName, 
+						"pose name passed correctly to sprite");
+					return {
+						draw: function (ctx, topLeftCorner) {
+							deepEqual(ctx, fakeCtx, 
+								"ctx passed correctly to pose.draw");
+							deepEqual(topLeftCorner, fakeTopLeft, 
+								"topLeftCorner passed correctly to pose.draw");
+						}
+					};
+				}
+
+			};
+			var initialState = "initialState";
+			var sprite = new renderers.SpriteRenderer(fakeAnimator, 
+	  												  fakeSprite, 
+	  												  initialState);
+			sprite.update(fakeTimeDelta);
+			equal(sprite.state, newState, "state was set correctly after update");
+
+			var fakeProps = {
+				shape: {},
+				forward: "right",
+				topLeftCorner: fakeTopLeft
+			};
+			sprite.draw(fakeCtx, fakeProps, fakeJsonLoader, fakeImageLoader);
+
+
+		});
+
     };
     return {run: run};
 }
